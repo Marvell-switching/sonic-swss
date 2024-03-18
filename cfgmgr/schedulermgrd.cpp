ab54e114167f62341afcf3fd9ac87d00050fa491
mazora <mazora@marvell.com>
2024-03-18 14:18:04 +0200

    First Commit
    created schedulermgrd and schedulermgr files

---
+++ cfgmgr/schedulermgrd.cpp
@@ -0,0 +1,118 @@
+#include <unistd.h>
+#include <getopt.h>
+#include <vector>
+#include <mutex>
+#include "dbconnector.h"
+#include "select.h"
+#include "exec.h"
+#include "schema.h"
+#include "schedulermgr.h"
+#include <fstream>
+#include <iostream>
+#include "json.h"
+#include <nlohmann/json.hpp>
+#include "warm_restart.h"
+
+using namespace std;
+using namespace swss;
+using json = nlohmann::json;
+
+/* SELECT() function timeout retry time, in millisecond */
+#define SELECT_TIMEOUT 1000
+
+void usage()
+{
+    cout << "Usage: schedulermgrd <-l pg_lookup.ini|-a asic_table.json [-p peripheral_table.json] [-z zero_profiles.json]>" << endl;
+    cout << "       -l pg_lookup.ini: PG profile look up table file (mandatory for static mode)" << endl;
+    cout << "           format: csv" << endl;
+    cout << "           values: 'speed, cable, size, xon,  xoff, dynamic_threshold, xon_offset'" << endl;
+    cout << "       -a asic_table.json: ASIC-specific parameters definition (mandatory for dynamic mode)" << endl;
+    cout << "       -p peripheral_table.json: Peripheral (eg. gearbox) parameters definition (optional for dynamic mode)" << endl;
+    cout << "       -z zero_profiles.json: Zero profiles definition for reclaiming unused buffers (optional for dynamic mode)" << endl;
+}
+
+
+int main(int argc, char **argv)
+{
+    int opt;
+
+    Logger::linkToDbNative("schedulermgrd");
+    SWSS_LOG_ENTER();
+
+    SWSS_LOG_NOTICE("--- Starting schedulermgrd ---");
+
+    while ((opt = getopt(argc, argv, "l:a:p:z:h")) != -1 )
+    {
+        switch (opt)
+        {
+        case 'l':
+            pg_lookup_file = optarg;
+            break;
+        case 'h':
+            usage();
+            return 1;
+        case 'a':
+            asic_table_file = optarg;
+            break;
+        case 'p':
+            peripherial_table_file = optarg;
+            break;
+        case 'z':
+            zero_profile_file = optarg;
+            break;
+        default: /* '?' */
+            usage();
+            return EXIT_FAILURE;
+        }
+    }
+
+    try
+    {
+        std::vector<Orch *> cfgOrchList;
+
+        DBConnector cfgDb("CONFIG_DB", 0);
+        DBConnector stateDb("STATE_DB", 0);
+
+
+        vector<string> cfg_buffer_tables = {
+            CFG_TIME_RANGE_TABLE_NAME,
+            CFG_SCHEDULED_CONFIGURATION_TABLE_NAME
+        };
+        cfgOrchList.emplace_back(new SchedulerMgr(&cfgDb, &stateDb, cfg_buffer_tables));
+
+        auto schedulermgr = cfgOrchList[0];
+
+        swss::Select s;
+        for (Orch *o : cfgOrchList)
+        {
+            s.addSelectables(o->getSelectables());
+        }
+
+        SWSS_LOG_NOTICE("starting main loop");
+        while (true)
+        {
+            Selectable *sel;
+            int ret;
+
+            ret = s.select(&sel, SELECT_TIMEOUT);
+            if (ret == Select::ERROR)
+            {
+                SWSS_LOG_NOTICE("Error: %s!", strerror(errno));
+                continue;
+            }
+            if (ret == Select::TIMEOUT)
+            {
+                schedulermgr->doTask();
+                continue;
+            }
+
+            auto *c = (Executor *)sel;
+            c->execute();
+        }
+    }
+    catch(const std::exception &e)
+    {
+        SWSS_LOG_ERROR("Runtime error: %s", e.what());
+    }
+    return -1;
+}
