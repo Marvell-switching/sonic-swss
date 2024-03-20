810b984cbc1623d295030c5409387ba7dedfb967
mazora <mazora@marvell.com>
2024-03-20 17:12:30 +0200

    Fixed bugs and Added Schedulermgr(d) to Makefile
    Fixed minor and return task_process_status
    Added systemd infrastructure functions to reload and update
    Removed options for schedulermgrd application

--- cfgmgr/schedulermgr.h
+++ cfgmgr/schedulermgr.h
@@ -10,8 +10,15 @@
 
 namespace swss {
 
-#define TIME_RANGE_ENABLED "enabled"
-#define TIME_RANGE_DISABLED "disabled"
+#define TIME_RANGE_ENABLED_STR "enabled"
+#define TIME_RANGE_DISABLED_STR "disabled"
+#define TIME_RANGE_STATUS_STR "status"
+
+#define SYSTEMD_FILES_PATH_PREFIX_STR "/etc/systemd/system/"
+#define SYSTEMD_UNIT_FILE_EXTENSION_STR ".service"
+#define SYSTEMD_TIMER_FILE_EXTENSION_STR ".timer"
+#define SYSTEMD_RELOAD_CMD_STR "systemctl daemon-reload"
+#define SYSTEMD_TIMER_ENABLE_PREFIX_CMD_STR "systemctl enable "
 
 class SchedulerMgr : public Orch
 {
@@ -25,11 +32,13 @@ private:
     Table m_cfgScheduledConfigurationTable;
 
     ProducerStateTable m_stateTimeRangeStatusTable;
-    
-    void writeSystemdUnitFile(string taskName, string taskType, string taskCommand);
-    void writeSystemdTimerFile(string taskName, string taskOnCalendar, bool isPersistent);
-    void writeSystemdFiles(string rangeName, string start, string end);
-    doTimeRangeTask doTimeRangeTask(string rangeName, vector<FieldValueTuple> &fieldValues);
+
+    bool reloadSystemd();
+    bool enableSystemdTimer(const std::string& timerFileName);
+    task_process_status writeSystemdUnitFile(const std::string& taskName, const std::string& taskType, const std::string& taskCommand);
+    task_process_status writeSystemdTimerFile(const std::string& taskName, const std::string& taskOnCalendar, const bool isPersistent);
+    task_process_status createSystemdFiles(const std::string& rangeName, const std::string& start, const std::string& end);
+    task_process_status doTimeRangeTask(const std::string& rangeName, const std::vector<FieldValueTuple>& fieldValues);
     void doTask(Consumer &consumer);
 };
 
