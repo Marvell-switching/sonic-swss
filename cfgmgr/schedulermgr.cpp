175468ceb30f0f6a971566259a031e45b5e54489
mazora <mazora@marvell.com>
2024-03-24 10:25:43 +0200

    Remove systemd and use cron instead

--- cfgmgr/schedulermgr.cpp
+++ cfgmgr/schedulermgr.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string.h>
+#include <sstream>
 #include "logger.h"
 #include "dbconnector.h"
 #include "producerstatetable.h"
@@ -16,8 +17,6 @@
 using namespace std;
 using namespace swss;
 
-#define PORT_NAME_GLOBAL "global"
-
 SchedulerMgr::SchedulerMgr(DBConnector *cfgDb, DBConnector *stateDb, const vector<string> &tableNames):
         Orch(cfgDb, tableNames),
         m_cfgTimeRangeTable(cfgDb, CFG_TIME_RANGE_TABLE_NAME),
@@ -25,99 +24,62 @@ SchedulerMgr::SchedulerMgr(DBConnector *cfgDb, DBConnector *stateDb, const vecto
         m_stateTimeRangeStatusTable(stateDb, STATE_TIME_RANGE_STATUS_TABLE_NAME)
 {}
 
-bool SchedulerMgr::reloadSystemd() {
-    // Reload systemd to recognize changes
-    return system(SYSTEMD_RELOAD_CMD_STR) == 0;
-}
-
-bool SchedulerMgr::enableSystemdTimer(const std::string& timerFileName) {
-    // Enable the systemd timer
-    string enableTimerCmd = SYSTEMD_TIMER_ENABLE_PREFIX_CMD_STR + timerFileName;
-    return system(enableTimerCmd.c_str()) == 0;
-}
-
-task_process_status SchedulerMgr::writeSystemdUnitFile(const string& fileName, const string& taskType, const string& taskCommand) {
-    string unitFilePath = SYSTEMD_FILES_PATH_PREFIX_STR + fileName;
-    ofstream unitFile{unitFilePath};
-    if (unitFile.fail()) {
-        SWSS_LOG_ERROR("Failed to create systemd file for %s", fileName.c_str());
+task_process_status SchedulerMgr::writeCrontabFile(const string& fileName, const string& schedule, const string& command, bool deleteSelfAfterCompletion) {
+    string cronFileName = CRON_FILES_PATH_PREFIX_STR + fileName;
+    ofstream crontabFile{cronFileName};
+    
+    if (crontabFile.fail()) {
+        SWSS_LOG_ERROR("Failed to create crontab file for %s", fileName.c_str());
         return task_process_status::task_need_retry;
     }
-    unitFile << "[Unit]\n";
-    unitFile << "Description=" << fileName << " service\n\n";
-    unitFile << "[Service]\n";
-    unitFile << "Type=" << taskType << "\n";
-    unitFile << "ExecStart=" << taskCommand << "\n";
-    unitFile.close();
-
-    SWSS_LOG_DEBUG("Systemd unit file for %s has been created", fileName.c_str());
-    return task_process_status::task_success;
-}
-
-task_process_status SchedulerMgr::writeSystemdTimerFile(const string& fileName, const string& taskOnCalendar, bool isPersistent) {
-    string timerFilePath = SYSTEMD_FILES_PATH_PREFIX_STR + fileName;
-    ofstream timerFile{timerFilePath};
-    if (timerFile.fail()) {
-        SWSS_LOG_ERROR("Failed to create systemd timer file for %s", fileName.c_str());
-        return task_process_status::task_need_retry;
+    crontabFile << schedule << " ";
+    crontabFile << CRON_USERNAME_STR << " ";
+    crontabFile << command;
+    if (deleteSelfAfterCompletion) {
+        crontabFile << " && rm " << cronFileName;
     }
-    timerFile << "[Unit]\n";
-    timerFile << "Description=Timer for " << fileName << " service\n\n";
-    timerFile << "[Timer]\n";
-    timerFile << "OnCalendar=" << taskOnCalendar << "\n";
-    if (isPersistent) {
-        timerFile << "Persistent=true\n";
-    }
-    timerFile << "\n";
-    timerFile << "[Install]\n";
-    timerFile << "WantedBy=timers.target\n";
-    timerFile.close();
+    crontabFile.close();
 
-    SWSS_LOG_DEBUG("Systemd timer file for %s has been created", fileName.c_str());
+    SWSS_LOG_DEBUG("Crontab file for %s has been created", fileName.c_str());
     return task_process_status::task_success;
 }
 
 // TODO add rollback mechanism
-task_process_status SchedulerMgr::createSystemdFiles(const string& taskName, const string& startTime, const string& endTime) {
-    // Service file for enabling the task
-    // TODO Replace with actual command 
-    string enableServiceFileName = taskName + "-enable" + SYSTEMD_UNIT_FILE_EXTENSION_STR;
-    if (writeSystemdUnitFile(enableServiceFileName, "oneshot", "/bin/echo 'Starting " + taskName + "'") != task_process_status::task_success) {
-        return task_process_status::task_need_retry;
+task_process_status SchedulerMgr::createCronjobs(const string& taskName, const string& startTime, const string& endTime, bool runOnce) {
+    string enableCrontabName = taskName + "-enable";
+    string disableCrontabName = taskName + "-disable";
+
+    // Create command for enabling the task
+    string command_enabled;
+    {
+        stringstream ss;
+        ss << "/usr/bin/redis-cli -n " << STATE_DB << " HSET '" << STATE_TIME_RANGE_STATUS_TABLE_NAME << "|" << taskName << "' '" << TIME_RANGE_STATUS_STR << "' '" << TIME_RANGE_ENABLED_STR << "'";
+        command_enabled = ss.str();
     }
 
-    // Timer file for enabling the task
-    string enableTimerFileName = taskName + "-enable" + SYSTEMD_TIMER_FILE_EXTENSION_STR;
-    if (writeSystemdTimerFile(enableTimerFileName, startTime, false) != task_process_status::task_success) {
+    // Create command for disabling the task
+    string command_disabled;
+    {
+        stringstream ss;
+        ss << "/usr/bin/redis-cli -n " << STATE_DB << " HSET '" << STATE_TIME_RANGE_STATUS_TABLE_NAME << "|" << taskName << "' '" << TIME_RANGE_STATUS_STR << "' '" << TIME_RANGE_DISABLED_STR << "'";
+        command_disabled = ss.str();
+    }
+
+    // Service file for enabling the task
+    // TODO Replace with actual command 
+    if (writecCrontabFile(enableCrontabName, endTime, command_enabled, runOnce) != task_process_status::task_success) {
         return task_process_status::task_need_retry;
     }
 
     // Service file for disabling the task
     // TODO Replace with actual command 
-    string disableServiceFileName = taskName + "-disable" + SYSTEMD_UNIT_FILE_EXTENSION_STR;
-    if (writeSystemdUnitFile(disableServiceFileName, "oneshot", "/bin/echo 'Stopping " + taskName + "'") != task_process_status::task_success) {
+    if (writecCrontabFile(disableCrontabName, endTime, command_disabled, runOnce) != task_process_status::task_success) {
         return task_process_status::task_need_retry;
     }
 
-    // Timer file for disabling the task
-    string disableTimerFileName = taskName + "-disable" + SYSTEMD_TIMER_FILE_EXTENSION_STR;
-    if (writeSystemdTimerFile(disableTimerFileName, endTime, false) != task_process_status::task_success) {
-        return task_process_status::task_need_retry;
-    } 
 
-    SWSS_LOG_INFO("Succesfully created Systemd files for %s", taskName.c_str());
+    SWSS_LOG_INFO("Succesfully created crontab files for %s", taskName.c_str());
 
-    // Reload systemd to pick up changes, then enable systemd timer files
-    if (!reloadSystemd()) {
-        SWSS_LOG_ERROR("Failed to reload systemd");
-        return task_process_status::task_need_retry;
-    }
-    if (!enableSystemdTimer(enableTimerFileName) || !enableSystemdTimer(disableTimerFileName)) {
-        SWSS_LOG_ERROR("Failed to enable systemd timer files for %s", taskName.c_str());
-        return task_process_status::task_need_retry;
-    }
-
-    SWSS_LOG_DEBUG("Succesfully reloaded and enabled systemd timer files for %s", taskName.c_str());
     return task_process_status::task_success;
 }
 
@@ -125,6 +87,7 @@ task_process_status SchedulerMgr::doTimeRangeTask(const string& rangeName, const
     SWSS_LOG_ENTER();
     string start = "";
     string end = "";
+    string runOnce = "";
 
     // Set time range status to disabled by default
     vector<FieldValueTuple> stateTableFieldValues;
@@ -137,6 +100,8 @@ task_process_status SchedulerMgr::doTimeRangeTask(const string& rangeName, const
             start = fvValue(i);
         } else if (fvField(i) == "end"){
             end = fvValue(i);
+        } else if (fvField(i) == "runOnce"){
+            runOnce = fvValue(i);
         } else {
             SWSS_LOG_ERROR("Time range %s has unknown field %s", rangeName.c_str(), fvField(i).c_str());
             // Can skip instead of returning invalid entry
@@ -151,7 +116,7 @@ task_process_status SchedulerMgr::doTimeRangeTask(const string& rangeName, const
 
     // Create systemd files for time range and enable them
     // TODO sanitize inputs
-    if (task_process_status::task_need_retry == createSystemdFiles(rangeName, start, end)) {
+    if (task_process_status::task_need_retry == createCronjobs(rangeName, start, end, (runOnce == "true"))) {
         return task_process_status::task_need_retry;
     }
     
--- cfgmgr/schedulermgr.h
+++ cfgmgr/schedulermgr.h
@@ -14,11 +14,10 @@ namespace swss {
 #define TIME_RANGE_DISABLED_STR "disabled"
 #define TIME_RANGE_STATUS_STR "status"
 
-#define SYSTEMD_FILES_PATH_PREFIX_STR "/etc/systemd/system/"
-#define SYSTEMD_UNIT_FILE_EXTENSION_STR ".service"
-#define SYSTEMD_TIMER_FILE_EXTENSION_STR ".timer"
-#define SYSTEMD_RELOAD_CMD_STR "systemctl daemon-reload"
-#define SYSTEMD_TIMER_ENABLE_PREFIX_CMD_STR "systemctl enable "
+
+#define CRON_FILES_PATH_PREFIX_STR "/etc/cron.d/"
+#define CRON_USERNAME_STR "root"
+
 
 class SchedulerMgr : public Orch
 {
@@ -33,11 +32,8 @@ private:
 
     ProducerStateTable m_stateTimeRangeStatusTable;
 
-    bool reloadSystemd();
-    bool enableSystemdTimer(const std::string& timerFileName);
-    task_process_status writeSystemdUnitFile(const std::string& taskName, const std::string& taskType, const std::string& taskCommand);
-    task_process_status writeSystemdTimerFile(const std::string& taskName, const std::string& taskOnCalendar, const bool isPersistent);
-    task_process_status createSystemdFiles(const std::string& rangeName, const std::string& start, const std::string& end);
+    task_process_status writeCrontabFile(const std::string& fileName, const std::string& schedule, const std::string& command, bool deleteSelfAfterCompletion);
+    task_process_status createCronjobs(const std::string& rangeName, const std::string& start, const std::string& end, bool runOnce);
     task_process_status doTimeRangeTask(const std::string& rangeName, const std::vector<FieldValueTuple>& fieldValues);
     void doTask(Consumer &consumer);
 };
