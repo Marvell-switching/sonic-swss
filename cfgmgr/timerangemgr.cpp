64a522083ff50fe0f1228c946df82e3cf5b81deb
mazora <mazora@marvell.com>
2024-05-16 15:22:04 +0300

    Added Multiple Updates (NOT TESTED)
    - Added 3 new files to help with cron validation & time checking. Leaning to use croncpp.h and removing cronhelper.cpp ands cronhelper.h
    - Added check that scheduled configuration does not already exist when adding a scheduled configuration
    - Automatically apply scheduled configuration if time range is currently active
    - Save scheduled configurations in "unboundConfiguration" hashmap when time range is deleted
    - Check if scheduled configurations saved in "unboundConfiguration" hashmap should be bound to newly created time range, and then add it to that new time range if applicable
    - Added if statement to not process time range/scheduled configuration if an invalid field is found in the asssociated table
    - Implemented DEL operation processing for scheduled configuration and time range deletion
    - New function "isTimeRangeActive()" in scheduledconfigmgr.cpp to check TIME_RANGE_STATUS table if the time range is active according to the field
    - New function "is_time_in_range()" that uses the croncpp.h file in order to test if the local time is within the scheduled time range interval -- NOT TESTED
    - Implemented DEL operation for when time range key is deleted from CONFIG_DB

--- cfgmgr/timerangemgr.cpp
+++ cfgmgr/timerangemgr.cpp
@@ -8,13 +8,29 @@
 #include "timerangemgr.h"
 #include "tokenize.h"
 #include <table.h>
+#include "croncpp.h"
 
 using namespace std;
 using namespace swss;
+using namespace cron;
 
 TimeRangeMgr::TimeRangeMgr(DBConnector *cfgDb, DBConnector *stateDb, const vector<string> &tableNames) : Orch(cfgDb, tableNames),
                                                                                                          m_stateTimeRangeStatusTable(stateDb, STATE_TIME_RANGE_STATUS_TABLE_NAME)
 {
+}
+
+
+bool is_time_in_range(const cronexpr& startExpr, const cronexpr& endExpr, const std::tm& currentTM) {
+    std::time_t currentTime = mktime(const_cast<tm*>(&currentTM)); // Convert currentTM to time_t
+    time_t previousMinute = currentTime - 60; // Go back one minute to ensure full coverage
+
+    // Convert back to tm
+    tm* prevTM = localtime(&previousMinute);
+
+    time_t nextStartTime = cron_previous(startExpr, mktime(prevTM));
+    time_t nextEndTime = cron_next(endExpr, mktime(prevTM));
+
+    return (currentTime >= nextStartTime && currentTime < nextEndTime);
 }
 
 task_process_status TimeRangeMgr::writeCrontabFile(const string &fileName, const string &schedule, const string &command, bool deleteSelfAfterCompletion)
@@ -75,6 +91,29 @@ task_process_status TimeRangeMgr::createCronjobs(const string &taskName, const s
 
     return task_process_status::task_success;
 }
+task_process_status TimeRangeMgr::doTimeRangeTaskDelete(const string &rangeName)
+{
+    SWSS_LOG_ENTER();
+    string enableCrontabName = rangeName + "-enable";
+    string disableCrontabName = rangeName + "-disable";
+
+    // Delete the crontab files for the time range
+    if (remove((CRON_FILES_PATH_PREFIX_STR + enableCrontabName).c_str()) != 0)
+    {
+        SWSS_LOG_ERROR("Failed to delete crontab file for %s", enableCrontabName.c_str());
+        return task_process_status::task_need_retry;
+    }
+    if (remove((CRON_FILES_PATH_PREFIX_STR + disableCrontabName).c_str()) != 0)
+    {
+        SWSS_LOG_ERROR("Failed to delete crontab file for %s", disableCrontabName.c_str());
+        return task_process_status::task_need_retry;
+    }
+
+    // Delete the time range status entry from the state db
+    m_stateTimeRangeStatusTable.del(rangeName);
+
+    return task_process_status::task_success;
+}
 
 task_process_status TimeRangeMgr::doTimeRangeTask(const string &rangeName, const vector<FieldValueTuple> &fieldValues)
 {
@@ -82,11 +121,6 @@ task_process_status TimeRangeMgr::doTimeRangeTask(const string &rangeName, const
     string start = "";
     string end = "";
     string runOnce = "";
-
-    // Set time range status to disabled by default
-    vector<FieldValueTuple> stateTableFieldValues;
-    string key = rangeName;
-    stateTableFieldValues.emplace_back(FieldValueTuple(TIME_RANGE_STATUS_STR, TIME_RANGE_DISABLED_STR));
 
     for (const auto &i : fieldValues)
     {
@@ -105,7 +139,6 @@ task_process_status TimeRangeMgr::doTimeRangeTask(const string &rangeName, const
         else
         {
             SWSS_LOG_ERROR("Time range %s has unknown field %s", rangeName.c_str(), fvField(i).c_str());
-            // Can skip instead of returning invalid entry
             return task_process_status::task_invalid_entry;
         }
     }
@@ -116,12 +149,34 @@ task_process_status TimeRangeMgr::doTimeRangeTask(const string &rangeName, const
         return task_process_status::task_invalid_entry;
     }
 
-    // Create systemd files for time range and enable them
+    // Create cron files for time range and enable them
     // TODO sanitize inputs
     if (task_process_status::task_need_retry == createCronjobs(rangeName, start, end, (runOnce == "true")))
     {
         return task_process_status::task_need_retry;
     }
+
+    // Check if time range should be active by default
+    auto startExpr = make_cron(start);
+    auto endExpr = make_cron(end);
+    tm currentTM;
+    string time_range_default_status = TIME_RANGE_DISABLED_STR;
+
+
+    time_t currentTime = time(nullptr);
+    localtime_r(&currentTime, &currentTM);
+
+
+    if (is_time_in_range(startExpr, endExpr, currentTM))
+    {
+        SWSS_LOG_INFO("Time range %s is active", rangeName.c_str());
+        time_range_default_status = TIME_RANGE_ENABLED_STR;
+    }
+
+    // Prepare state table field-values
+    vector<FieldValueTuple> stateTableFieldValues;
+    string key = rangeName;
+    stateTableFieldValues.emplace_back(FieldValueTuple(TIME_RANGE_STATUS_STR, time_range_default_status));
 
     // Add time range status to range status table in state db
     m_stateTimeRangeStatusTable.set(key, stateTableFieldValues);
@@ -151,6 +206,13 @@ void TimeRangeMgr::doTask(Consumer &consumer)
             if (table_name == CFG_TIME_RANGE_TABLE_NAME)
             {
                 task_status = doTimeRangeTask(rangeName, kfvFieldsValues(t));
+            }
+        }
+        else if (op == DEL_COMMAND)
+        { 
+            if (table_name == CFG_TIME_RANGE_TABLE_NAME)
+            {
+                task_status = doTimeRangeTaskDelete(rangeName);
             }
         }
         switch (task_status)
